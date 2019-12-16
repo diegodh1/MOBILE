0e6c22858e79d9a159b5666d6b69475e26ab2445
unknown <diegodiazh1994@gmail.com>
2019-12-15 19:23:58 -0500

    mobile v.1

---
+++ APKFUNCIONANDOJQUERY/www/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	var screen = $.mobile.getScreenHeight(),
+    header = $(".ui-header").hasClass("ui-header-fixed") ? $(".ui-header").outerHeight() - 1 : $(".ui-header").outerHeight(),
+    footer = $(".ui-footer").hasClass("ui-footer-fixed") ? $(".ui-footer").outerHeight() - 1 : $(".ui-footer").outerHeight(),
+    contentCurrent = $(".ui-content").outerHeight() - $(".ui-content").height(),
+    content = screen - header - footer - contentCurrent;
+$(".ui-content").height(content);
+});
